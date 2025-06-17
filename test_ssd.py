2a23e99568e8b431a4b795d21c1bd89b414c603a
youngchul.hwang <sofe072@gmail.com>
2025-06-17 16:42:14 +0900

    [test] add 'test_write_data'

--- test_ssd.py
+++ test_ssd.py
@@ -7,6 +7,11 @@ LBA_LENGTH = 100
 NAND_FILE = "test_ssd_nand.txt"
 OUTPUT_FILE = "test_ssd_output.txt"
 EMPTY_DATA = "0x00000000"
+WRITE_COMMAND = "W"
+READ_COMMAND = "R"
+
+
+
 
 def remove_files():
     if os.path.exists(NAND_FILE):
@@ -18,6 +23,11 @@ def assert_output_file(expected):
     with open(OUTPUT_FILE, 'r') as f:
         content = f.read()
         assert content == expected
+
+def check_write_and_read(_ssd, addr, wdata):
+    _ssd.run([WRITE_COMMAND, addr, wdata])
+    _ssd.run([READ_COMMAND, addr])
+    assert_output_file(wdata)
 
 @pytest.fixture
 def clean_ssd():
@@ -53,9 +63,20 @@ def test_initial_output_file(clean_ssd):
         content = f.read()
         assert content == ""
 
-
+@pytest.mark.skip
 def test_read_empty_data(clean_ssd):
-    clean_ssd.run(["R", "0"])
+    """
+    0 번 주소, 마지막 주소 read 해서 0x00000000 읽히는 지 확인
+    """
+    clean_ssd.run([READ_COMMAND, "0"])
     assert_output_file(EMPTY_DATA)
-    clean_ssd.run(["R", f"{LBA_LENGTH-1}"])
+    clean_ssd.run([READ_COMMAND, f"{LBA_LENGTH-1}"])
     assert_output_file(EMPTY_DATA)
+
+@pytest.mark.skip
+def test_write_data(clean_ssd):
+    """
+    0번 주소, 마지막 주소 write 해서 wdata가 읽히는 지 확인
+    """
+    check_write_and_read(clean_ssd, "0", "0x1234ABCD")
+    check_write_and_read(clean_ssd, f"{LBA_LENGTH - 1}", "0xAAAA5555")
