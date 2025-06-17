db631dca41be88eac5933352ed69ca1e7f82126b
sanghyung.kim <sh03.kim@samsung.com>
2025-06-18 08:40:36 +0900

    feat: add read mock test

--- test_shell.py
+++ test_shell.py
@@ -1,2 +1,11 @@
 import pytest
+from pytest_mock import MockerFixture
+
+import shell
+
+
+def test_shell_read(mocker: MockerFixture):
+    mocker.patch('shell.read', return_value='0x99999999')
+    result = shell.read("3")
+    assert result == "0x99999999"
 
