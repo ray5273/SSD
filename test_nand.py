77931093fc11df447fd3a3ed195e9beefc83249d
yeonho.jeong <yeonho0701@naver.com>
2025-06-18 09:22:18 +0900

    [test] initial test_nand.py test 용 file_path를 분리하였으며, data 형식을 소문자로 통일

--- test_nand.py
+++ test_nand.py
@@ -2,7 +2,7 @@ import pytest
 
 from nand import Nand
 
-FILE_PATH = "ssd_nand.txt"
+FILE_PATH = "test_ssd_nand.txt"
 LBA_LENGTH = 100
 DEFAULT_VALUE = "0x00000000"
 FIRST_ADDRESS = 0
@@ -20,13 +20,13 @@ def test_nand_read_uninitialized_data_at_last_address(nand):
 
 def test_nand_write_first_address(nand):
     addr = FIRST_ADDRESS
-    wdata = "0x1234ABCD"
+    wdata = "0x1234abcd"
     nand.write(addr, wdata)
     assert nand.read(addr) == wdata
 
 def test_nand_write_last_address(nand):
     addr = LAST_ADDRESS
-    wdata = "0x1F1F1F1F"
+    wdata = "0x1f1f1f1f"
     nand.write(addr, wdata)
     assert nand.read(addr) == wdata
 
