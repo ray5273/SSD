e4fa099a7716687c9ccbd474fc5a513cb0a06aee
sanghyung.kim <sh03.kim@samsung.com>
2025-06-18 09:15:42 +0900

    feat: read result file(ssd_output.txt) 구현 및 테스트

--- test_shell.py
+++ test_shell.py
@@ -1,9 +1,10 @@
 
+import os
 import pytest
 from pytest_mock import MockerFixture
 
 import shell
-
+import tempfile
 
 def test_shell_read(mocker: MockerFixture):
     mocker.patch('shell.read', return_value='0x99999999')
@@ -14,9 +15,18 @@ def test_call_system():
     cmd = f'dir'
     assert shell.call_system(cmd) == 0
 
-@pytest.mark.skip
 def test_read_result_file():
-    ...
+    TEST_DATA = '0x99999999'
+    # 시스템 임시 디렉터리 경로
+    tmp_dir = tempfile.gettempdir()
+    # 내가 지정한 임시 파일명
+    filename = "ssd_output.txt"
+    file_path = os.path.join(tmp_dir, filename)
+    # 임시 파일 생성 및 쓰기
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(TEST_DATA)
+
+    assert shell.read_result_file(file_path) == TEST_DATA
 
 @pytest.mark.skip
 def test_read_with_valid_lba():
