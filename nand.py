fab2527169d216862bf3b6ea8a7471b8cb758ee5
jaehoon <lee107601@naver.com>
2025-06-18 14:04:54 +0900

    refactoring: indent 간소화 및 magic number 제거

--- nand.py
+++ nand.py
@@ -1,27 +1,30 @@
 import os
 
+DEFAULT_DATA = "0x00000000"
+
 
 class Nand:
     def __init__(self, lba_length, data_file):
         self.data_file = data_file
         self.lba_length = lba_length
-        self.default_value = "0x00000000"
+        self.default_data = DEFAULT_DATA
 
     def create_data_file(self):
         with open(self.data_file, 'a', encoding='utf-8') as f:
-            for i in range(self.lba_length):
-                f.write(f"{self.default_value}\n")
+            for index in range(self.lba_length):
+                f.write(f"{self.default_data}\n")
 
     def read(self, address) -> str:
         if not os.path.exists(self.data_file):
             self.create_data_file()
 
         with open(self.data_file, 'r', encoding='utf-8') as f:
-            for i, line in enumerate(f, start=0):
-                if i == address:
-                    value = line.strip()
-                    return value
-        raise Exception
+            lines = f.readlines()
+
+        if address > len(lines):
+            raise Exception
+
+        return lines[address].strip()
 
     def write(self, address, wdata):
         if not os.path.exists(self.data_file):
@@ -30,9 +33,10 @@ class Nand:
         with open(self.data_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
-        if address <= len(lines):
-            lines[address] = wdata + '\n'
-            with open(self.data_file, 'w', encoding='utf-8') as f:
-                f.writelines(lines)
-        else:
+        if address > len(lines):
             raise Exception
+
+        lines[address] = wdata + '\n'
+
+        with open(self.data_file, 'w', encoding='utf-8') as f:
+            f.writelines(lines)
