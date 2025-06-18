1d68cd98d321e6586be63e3aa6f2eb606fe38346
jaehoon <lee107601@naver.com>
2025-06-18 09:35:34 +0900

    feature: create Nand class

---
+++ nand.py
@@ -0,0 +1,11 @@
+class Nand:
+    def __init__(self, data_file, lba_length):
+        self.data_file = data_file
+        self.lba_length = lba_length
+
+    def read(self, address) -> str:
+        pass
+
+    def write(self, address, value):
+        pass
+
