fc7b685e90c6d8399fcb97a29efa9f590e538c01
jaehoon <lee107601@naver.com>
2025-06-18 11:08:56 +0900

    fix: 프로그램 재실행시 데이터파일 유지되도록 수정

--- nand.py
+++ nand.py
@@ -1,17 +1,21 @@
+import os
+
+
 class Nand:
     def __init__(self, lba_length, data_file):
         self.data_file = data_file
         self.lba_length = lba_length
         self.default_value = "0x00000000"
 
-        with open(self.data_file, 'w', encoding='utf-8') as f:
-            pass
-
+    def create_data_file(self):
         with open(self.data_file, 'a', encoding='utf-8') as f:
-            for i in range(lba_length):
+            for i in range(self.lba_length):
                 f.write(f"{self.default_value}\n")
 
     def read(self, address) -> str:
+        if not os.path.exists(self.data_file):
+            self.create_data_file()
+
         with open(self.data_file, 'r', encoding='utf-8') as f:
             for i, line in enumerate(f, start=0):
                 if i == address:
@@ -20,6 +24,9 @@ class Nand:
         raise Exception
 
     def write(self, address, wdata):
+        if not os.path.exists(self.data_file):
+            self.create_data_file()
+
         with open(self.data_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
