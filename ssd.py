466c5e124c58e66b87e7086d6dd68e7bc9e93ec5
ezitsu <ezitsu@gmail.com>
2025-06-18 10:50:51 +0900

    feat: FileOutput과 validator를 쓰도록 수정 (Test 통과 전 임시 commit)

--- ssd.py
+++ ssd.py
@@ -1,58 +1,28 @@
-import os
-import sys
-from abc import ABC, abstractmethod
-
-class ICommand(ABC):
-    def __init__(self, argv, input_fh, output_fh):
-        self.validate_command(argv)
-
-    @abstractmethod
-    def validate_command(self):
-        ...
-
-    @abstractmethod
-    def execute(self):
-        ...
-
-    def is_hex(self, data):
-        ...
-
-class ReadCommand(ICommand):
-    ...
-
-class WriteCommand(ICommand):
-    ...
+from file_output import FileOutput
+from lba_validator import LBAValidator
 
 
 class SSD:
-    NAND_FILE = "ssd_nand.txt"
-    OUTPUT_FILE = "ssd_output.txt"
     LBA_LENGTH = 100
 
-    def __init__(self):
-        self.init_ssd_nand_file()
-        self.init_sdd_output_file()
+    def __init__(self, device):
+        self._device = device
+        self._out_writer = FileOutput()
+        self._last_result = None
+        self._param_validator = LBAValidator(self.LBA_LENGTH)
 
-    def init_ssd_nand_file(self):
-        if not os.path.exists(self.NAND_FILE):
-            with open(self.NAND_FILE, "w") as f:
-                f.write("")
-            for lba in range(self.LBA_LENGTH):
-                # WriteCommand(self.NAND_FILE, self.output_file, lba, 0x00000000).execute()
-                pass
+    def run(self, params: list) -> bool:
+        if not self._param_validator.is_valid(params):
+            return False
 
-    def init_ssd_output_file(self):
-        if not os.path.exists(self.OUTPUT_FILE):
-            with open(self.OUTPUT_FILE, "w") as f:
-                f.write("")
-
-    def run(self, argv):
-        if argv[0] == "R":
-            cmd = ReadCommand(argv[1:])
-        elif argv[0] == "W":
-            cmd = WriteCommand(argv[1:])
-        cmd.execute()
-
-
-if __name__ == "__main__":
-    SSD().run(sys.argv[1:])
+        command, address = params[0], params[1]
+        try:
+            if command == "R":
+                self._last_result = self._device.read(address)
+                self._out_writer.write(self._last_result)
+            if command == "W":
+                value = params[2]
+                self._device.write(address, value)
+        except Exception as e:
+            return False
+        return True
