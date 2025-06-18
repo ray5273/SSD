44677b32fb9be53a1438adee56425758b1e7cffb
yeonho7 <yeonho0701@naver.com>
2025-06-18 09:31:02 +0900

    [test] initial test_nand.py
    ssd 별도로 nand test case 추가
    
    기본 read/write와 addr exception case 추가

---
+++ test_nand.py
@@ -0,0 +1,42 @@
+import pytest
+
+from nand import Nand
+
+FILE_PATH = "test_ssd_nand.txt"
+LBA_LENGTH = 100
+DEFAULT_VALUE = "0x00000000"
+FIRST_ADDRESS = 0
+LAST_ADDRESS = LBA_LENGTH - 1
+
+@pytest.fixture
+def nand():
+    return Nand(LBA_LENGTH, FILE_PATH)
+
+def test_nand_read_uninitialized_data_at_start_address(nand):
+    assert nand.read(FIRST_ADDRESS) == DEFAULT_VALUE
+
+def test_nand_read_uninitialized_data_at_last_address(nand):
+    assert nand.read(LAST_ADDRESS) == DEFAULT_VALUE
+
+def test_nand_write_first_address(nand):
+    addr = FIRST_ADDRESS
+    wdata = "0x1234abcd"
+    nand.write(addr, wdata)
+    assert nand.read(addr) == wdata
+
+def test_nand_write_last_address(nand):
+    addr = LAST_ADDRESS
+    wdata = "0x1f1f1f1f"
+    nand.write(addr, wdata)
+    assert nand.read(addr) == wdata
+
+def test_nand_read_out_of_bounds(nand):
+    addr = 999999
+    with pytest.raises(Exception):
+        nand.read(addr)
+
+def test_nand_write_out_of_bounds(nand):
+    addr = 1000000
+    wdata = "0x00000000"
+    with pytest.raises(Exception):
+        nand.write(addr, wdata)
