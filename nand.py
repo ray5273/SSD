5cab95db658c7d916a6796d1d5e8ac20ea6b2e38
jaehoon <lee107601@naver.com>
2025-06-18 14:52:31 +0900

    Fix: raise 조건 lba 경계값 수정

--- nand.py
+++ nand.py
@@ -9,31 +9,29 @@ class Nand:
         self.lba_length = lba_length
         self.default_data = DEFAULT_DATA
 
+        if not os.path.exists(self.data_file):
+            self.create_data_file()
+
     def create_data_file(self):
         with open(self.data_file, 'a', encoding='utf-8') as f:
             for index in range(self.lba_length):
                 f.write(f"{self.default_data}\n")
 
     def read(self, address) -> str:
-        if not os.path.exists(self.data_file):
-            self.create_data_file()
-
         with open(self.data_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
-        if address > len(lines):
+        if address >= len(lines):
             raise Exception
 
         return lines[address].strip()
 
     def write(self, address, wdata):
-        if not os.path.exists(self.data_file):
-            self.create_data_file()
 
         with open(self.data_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
-        if address > len(lines):
+        if address >= len(lines):
             raise Exception
 
         lines[address] = wdata + '\n'
