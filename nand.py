289836b3800fd523b3cee327752b8a521149f63d
jaehoon <lee107601@naver.com>
2025-06-18 09:42:37 +0900

    feature: add init method

--- nand.py
+++ nand.py
@@ -3,8 +3,16 @@ class Nand:
         self.data_file = data_file
         self.lba_length = lba_length
 
+        with open(self.data_file, 'w', encoding='utf-8') as f:
+            pass
+
+        with open(self.data_file, 'a', encoding='utf-8') as f:
+            for i in range(self.lba_length):
+                f.write(f"{self.lba_length}\n")
+
+
     def read(self, address) -> str:
-        pass
+
 
     def write(self, address, value):
         pass
