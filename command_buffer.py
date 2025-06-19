55e1b057d44c6d7e9c64079b2d4f1eeaa08d2f05
jaehoon <lee107601@naver.com>
2025-06-19 18:39:09 +0900

    feat: ignore write 기능 구현

--- command_buffer.py
+++ command_buffer.py
@@ -2,8 +2,8 @@ import pytest
 from nand import Nand
 from buffer_driver import BufferDriver
 
-
 DEFAULT_VALUE = "0x00000000"
+
 
 class CommandBuffer():
     def __init__(self, device: Nand):
@@ -30,16 +30,39 @@ class CommandBuffer():
     def get_data_in_buffer(self, buffers, address):
         for command in reversed(buffers):
             if self.is_contained_range(command, address):
-                if command[0]=='E':
+                if command[0] == 'E':
                     return DEFAULT_VALUE
                 else:
                     write_data = command[2]
                     return write_data
         return DEFAULT_VALUE
 
+    def ignore_write(self, buffers):
+        size = len(buffers)
+        delete_index_list = []
+        for i in range(size - 1, -1, -1):
+            commands = buffers[i]
+            command = commands[0]
+            lba = commands[1]
+            count_or_data = commands[2]
+            if command == "W":
+                continue
+            start_address = lba
+            end_address = lba + count_or_data - 1
+            for j in range(size - 1, -1, -1):
+                commands = buffers[j]
+                command_left = commands[0]
+                lba_left = commands[1]
+                count_or_data_left = commands[2]
+                if command_left == "E":
+                    continue
+                if lba_left >= start_address and lba_left <= end_address:
+                    delete_index_list.append(j)
+        for delete_index in delete_index_list:
+            del buffers[delete_index]
 
     def optimize(self, buffers):
-        return
+        pass
 
     def read(self, address) -> str:
         buffers = self._driver.get_list_from_buffer_files()
@@ -49,6 +72,8 @@ class CommandBuffer():
         return self._device.read(address)
 
     def write(self, address, wdata):
+        if wdata == "0x00000000":
+            self.erase(address, 1)
         buffers = self._driver.get_list_from_buffer_files()
         if len(buffers) == 5:
             self.flush()
@@ -58,6 +83,8 @@ class CommandBuffer():
         self._driver.make_buffer_files_from_list(buffers)
 
     def erase(self, start_address, size):
+        if size <= 0:
+            return
         buffers = self._driver.get_list_from_buffer_files()
         if len(buffers) == 5:
             self.flush()
