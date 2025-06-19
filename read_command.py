d8392fe4a71d2dbd273de9ab8993fd892bc86b22
Jeesu Lee <ezitsu@gmail.com>
2025-06-19 15:15:48 +0900

    refactor: 기능별로 CommandInterface 상속 받아서 구현하도록 변경 (기능 확장성 고려) (#40)

---
+++ read_command.py
@@ -0,0 +1,18 @@
+from command import Command
+from command_interface import SSDCommandInterface
+
+
+class ReadSSDCommand(SSDCommandInterface):
+    def __init__(self, device, output_writer):
+        super().__init__(device, output_writer, Command.READ, 2)
+
+    def is_valid_param(self, params: list) -> bool:
+        return self.common_param_validation(params)
+
+    def execute(self, params) -> str:
+        if not self.is_valid_param(params):
+            self.result = self.ERROR_MSG
+        else:
+            address = int(params[1])
+            self.result = self._device.read(address)
+        return self.result
---
+++ write_command.py
@@ -0,0 +1,38 @@
+from command import Command
+from command_interface import SSDCommandInterface
+
+
+class WriteSSDCommand(SSDCommandInterface):
+    VALUE_LENGTH = 10
+
+    def __init__(self, device, output_writer):
+        super().__init__(device, output_writer, Command.WRITE, 3)
+
+
+    def is_valid_param(self, params: list) -> bool:
+        if not self.common_param_validation(params):
+            return False
+
+        value = params[2]
+        if len(value) != self.VALUE_LENGTH:
+            return False
+
+        try:
+            _ = int(value, 16)
+        except ValueError:
+            return False
+
+        return True
+
+    def execute(self, params) -> str:
+        if not self.is_valid_param(params):
+            self.result = self.ERROR_MSG
+        else:
+            try:
+                address, value = int(params[1]), params[2]
+                self._device.write(address, value)
+                self.result = ""
+            except Exception:
+                self.result = self.ERROR_MSG
+
+        return self.result
