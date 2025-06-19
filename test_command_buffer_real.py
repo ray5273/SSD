4207fb66c2165f5ce57994237484b27168decda5
jaehoon <lee107601@naver.com>
2025-06-19 18:04:54 +0900

    Test : command buffer 실 데이터 테스트 추가

---
+++ test_command_buffer_real.py
@@ -0,0 +1,66 @@
+import pytest
+from command_buffer import CommandBuffer
+from buffer_driver import BufferDriver
+from nand import Nand
+
+FILE_PATH = "test_ssd_nand.txt"
+LBA_LENGTH = 100
+DEFAULT_VALUE = "0x00000000"
+FIRST_ADDRESS = 0
+LAST_ADDRESS = LBA_LENGTH - 1
+
+@pytest.fixture
+def command_buffer():
+    nand = Nand(LBA_LENGTH, FILE_PATH)
+    return CommandBuffer(nand)
+
+
+def test_read_command(command_buffer):
+    command_buffer.write(1,'0x00001234')
+    assert command_buffer.read(1) == '0x00001234'
+
+def test_erase_read_command(command_buffer):
+    command_buffer.write(2,'0x00001234')
+    command_buffer.write(3,'0x00001234')
+    command_buffer.write(1,'0x00001234')
+    command_buffer.erase(1,5)
+    assert command_buffer.read(1) == '0x00000000'
+    assert command_buffer.read(2) == '0x00000000'
+    assert command_buffer.read(3) == '0x00000000'
+    assert command_buffer.read(4) == '0x00000000'
+    assert command_buffer.read(5) == '0x00000000'
+
+
+def test_flush(command_buffer):
+    command_buffer.write(2,'0x00001234')
+    command_buffer.write(3,'0x00001234')
+    command_buffer.write(1,'0x00001234')
+    command_buffer.write(4,'0x00001234')
+    command_buffer.write(5,'0x00001234')
+    command_buffer.write(6,'0x00001234')
+    command_buffer.write(7,'0x00001234')
+    command_buffer.write(8,'0x00001234')
+    command_buffer.write(9,'0x00001234')
+    assert command_buffer.read(1) == '0x00001234'
+    assert command_buffer.read(2) == '0x00001234'
+    assert command_buffer.read(3) == '0x00001234'
+    assert command_buffer.read(4) == '0x00001234'
+    assert command_buffer.read(5) == '0x00001234'
+    assert command_buffer.read(6) == '0x00001234'
+    assert command_buffer.read(7) == '0x00001234'
+    assert command_buffer.read(8) == '0x00001234'
+    assert command_buffer.read(9) == '0x00001234'
+    command_buffer.flush()
+    assert command_buffer.read(1) == '0x00001234'
+    assert command_buffer.read(2) == '0x00001234'
+    assert command_buffer.read(3) == '0x00001234'
+    assert command_buffer.read(4) == '0x00001234'
+    assert command_buffer.read(5) == '0x00001234'
+    assert command_buffer.read(6) == '0x00001234'
+    assert command_buffer.read(7) == '0x00001234'
+    assert command_buffer.read(8) == '0x00001234'
+    assert command_buffer.read(9) == '0x00001234'
+
+
+
+
