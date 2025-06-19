8c1d1cc1ebc48a450215d21d4411be85bf6ecade
jaehoon <lee107601@naver.com>
2025-06-19 15:21:44 +0900

    Refactor: 함수이름 변경 및 매직넘버 제거

--- test_buffer_driver.py
+++ test_buffer_driver.py
@@ -8,20 +8,20 @@ def buffer_driver():
 
 def test_get_list_from_buffer_empty_files(buffer_driver):
     list = []
-    buffer_driver.make_list_to_buffer_files(list)
+    buffer_driver.make_buffer_files_from_list(list)
     assert buffer_driver.get_list_from_buffer_files() == []
 
 def test_get_list_from_buffer_command_files(buffer_driver):
     command = ('W','0','0x00000000')
     list = [command]
-    buffer_driver.make_list_to_buffer_files(list)
+    buffer_driver.make_buffer_files_from_list(list)
     assert buffer_driver.get_list_from_buffer_files() == [command]
 
 def test_get_list_from_buffer_command_files2(buffer_driver):
     command1 = ('W','0','0x00000001')
     command2 = ('W','0','0x00000002')
     list = [command1, command2]
-    buffer_driver.make_list_to_buffer_files(list)
+    buffer_driver.make_buffer_files_from_list(list)
     assert buffer_driver.get_list_from_buffer_files() == list
 
 
@@ -34,6 +34,6 @@ def test_make_list_to_buffer_file_fail(buffer_driver):
     command6 = ('W','0','0x00000006')
     list = [command1, command2,command3,command4,command5,command6]
 
-    assert buffer_driver.make_list_to_buffer_files(list) == None
+    assert buffer_driver.make_buffer_files_from_list(list) == None
 
 
