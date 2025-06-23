b2f1a0d5b796e684d69f375f2220b520a1f644fe
oHo <yeonho0701@naver.com>
2025-06-24 00:59:29 +0900

    [refactor] command_buffer 의 write, erase buffer 관련 내용 추출하여 Bufferable class 구현

---
+++ bufferables.py
@@ -0,0 +1,87 @@
+from abc import abstractmethod, ABC
+from constants import DEFAULT_DATA
+
+class Bufferable(ABC):
+    device = None
+    def __init__(self):
+        self.need_to_ignore = False
+
+    @classmethod
+    def is_write(cls):
+        return False
+
+    @classmethod
+    def is_erase(cls):
+        return True
+
+    @abstractmethod
+    def contains_address(self, addr):
+        ...
+
+    @abstractmethod
+    def get_data(self, addr):
+        ...
+
+    @abstractmethod
+    def to_tuple(self):
+        ...
+
+    @abstractmethod
+    def execute(self):
+        ...
+
+
+class WriteBuffer(Bufferable):
+    def __init__(self, cmd, addr, data):
+        super().__init__()
+        self.addr = addr
+        self.data = data
+
+    @classmethod
+    def is_write(cls):
+        return True
+
+    def contains_address(self, addr):
+        return addr == self.addr
+
+    def get_data(self, addr):
+        return self.data
+
+    def to_tuple(self):
+        return ('W', self.addr, self.data)
+
+    def execute(self):
+        self.device.write(self.addr, self.data)
+
+
+class EraseBuffer(Bufferable):
+    def __init__(self, cmd, start_addr, size):
+        super().__init__()
+        self.start_addr = start_addr
+        self.size = size
+        self.end_addr = start_addr + size -1
+        self.overwrite_table = [ False ] * size
+
+    @classmethod
+    def is_erase(cls):
+        return True
+
+    def mark_overwritten(self, addr):
+        self.overwrite_table[addr - self.start_addr] = True
+
+    def is_all_overwritten(self):
+        return all(self.overwrite_table)
+
+    def contains_address(self, addr):
+        return self.start_addr <= addr <= self.end_addr
+
+    def get_data(self, addr):
+
+        return DEFAULT_DATA
+
+    def execute(self):
+        self.device.erase(self.start_addr, self.size)
+
+    def to_tuple(self):
+        return ('E', self.start_addr, self.size)
+
--- command_buffer.py
+++ command_buffer.py
@@ -1,4 +1,7 @@
-import pytest
+from typing import List, Union, Optional
+
+from constants import DEFAULT_DATA
+from bufferables import Bufferable, WriteBuffer, EraseBuffer
 from nand import Nand
 from buffer_driver import BufferDriver
 import copy
@@ -6,226 +9,166 @@ import copy
 DEFAULT_VALUE = "0x00000000"
 ERASE_MAX_SIZE = 10
 
-
 class CommandBuffer:
     def __init__(self, device: Nand):
         self._device = device
         self._driver = BufferDriver()
-        self._buffers = self._driver.get_list_from_buffer_files()
-
-    def is_command_write(self, command: tuple):
-        return command[0] == 'W'
-
-    def is_command_erase(self, command: tuple):
-        return command[0] == 'E'
-
-    def get_op_from_command(self, command: tuple):
-        return command[0]
-
-    def get_lba_from_command(self, command: tuple):
-        return command[1]
-
-    def get_count_or_data(self, command: tuple):
-        return command[2]
+        self.buffers = self._driver.get_list_from_buffer_files()
+        Bufferable.device = device
 
     @property
     def buffers(self):
-        return self._buffers
+        return self.bufferables_to_list(self._buffers)
+        # return self._buffers
 
     @buffers.setter
     def buffers(self, datas: list):
-        self._buffers = datas
+        self._buffers = self.list_to_bufferables(datas)
+        # self._buffers = datas
+
+    def list_to_bufferables(self, param_list: List[tuple]) -> List[Union[WriteBuffer, EraseBuffer]]:
+        buffers = []
+        for params in param_list:
+            if params[0] == 'W':
+                buffers.append(WriteBuffer(*params))
+            elif params[0] == 'E':
+                buffers.append(EraseBuffer(*params))
+        return buffers
+
+    def bufferables_to_list(self, buffers) -> List[tuple]:
+        return [x.to_tuple() for x in buffers ]
 
     def is_buffers_full(self):
         return len(self._buffers) == 5
 
-    def is_contained_range(self, command: tuple, address):
-        start_address = self.get_lba_from_command(command)
-        if self.is_command_write(command):
-            end_address = start_address
-        else:
-            end_address = start_address + self.get_count_or_data(command) - 1
-        return start_address <= address <= end_address
+    def get_data_in_buffer(self, address) -> Optional[str]:
+        for buffer in reversed(self._buffers):
+            if buffer.contains_address(address):
+                return buffer.get_data(address)
+        return None
 
-    def is_exist_in_buffer(self, address):
-        if len(self._buffers) == 0:
-            return False
-
-        for command in reversed(self._buffers):
-            if self.is_contained_range(command, address):
-                return True
-
-        return False
-
-    def get_data_in_buffer(self, address):
-        for command in reversed(self._buffers):
-            if self.is_contained_range(command, address):
-                if self.is_command_erase(command):
-                    return DEFAULT_VALUE
-                else:
-                    write_data = command[2]
-                    return write_data
-        return DEFAULT_VALUE
+    def add_buffer(self, buffer):
+        if self.is_buffers_full():
+            self.flush()
+        self._buffers.append(buffer)
+        self.optimize()
+        self._driver.make_buffer_files_from_list(self.buffers)
 
     def ignore_write(self, buffers):
         size = len(buffers)
-        delete_index_set = set()
+        buffers = self.list_to_bufferables(buffers)
         for i in range(size - 1, 0, -1):
-            command = buffers[i]
-            lba = self.get_lba_from_command(command)
-            count_or_data = self.get_count_or_data(command)
-            if self.is_command_write(command):
-                continue
-            start_address = lba
-            end_address = lba + count_or_data - 1
+            right_buffer = buffers[i]
+            # if right_buffer.is_write():
+            #     continue
             for j in range(i - 1, -1, -1):
-                command = buffers[j]
-                lba_left = self.get_lba_from_command(command)
-                if self.is_command_erase(command):
+                left_write_buffer = buffers[j]
+                if left_write_buffer.is_erase():
                     continue
-                if start_address <= lba_left <= end_address:
-                    delete_index_set.add(j)
+                if right_buffer.contains_address( left_write_buffer.addr ):
+                    left_write_buffer.need_to_ignore = True
 
-        result_buffers = []
-        for index, buffer in enumerate(buffers):
-            if index in delete_index_set:
-                continue
-            result_buffers.append(buffer)
-        return result_buffers
+        result_buffers = [ x for x in buffers if x.need_to_ignore == False]
+        return self.bufferables_to_list(result_buffers)
 
     def ignore_erase(self, buffers):
         size = len(buffers)
-        delete_index_set = set()
+        buffers = self.list_to_bufferables(buffers)
         for i in range(0, size - 1):
-            command = buffers[i]
-            lba = self.get_lba_from_command(command)
-            count_or_data = self.get_count_or_data(command)
-            if self.is_command_write(command):
+            left_erase_buffer = buffers[i]
+            if left_erase_buffer.is_write():
                 continue
-            start_address = lba
-            end_address = lba + count_or_data - 1
-
-            is_write = [False] * count_or_data
-
             for j in range(i + 1, size):
-                command = buffers[j]
-                lba_right = self.get_lba_from_command(command)
-                if self.is_command_erase(command):
+                right_write_buffer = buffers[j]
+                if right_write_buffer.is_erase():
                     continue
-                if start_address <= lba_right <= end_address:
-                    is_write[lba_right - start_address] = True
+                if left_erase_buffer.contains_address(right_write_buffer.addr):
+                    left_erase_buffer.mark_overwritten(right_write_buffer.addr)
 
-            is_ignore_erase_possible = True
-            for k in range(count_or_data):
-                if is_write[k] is False:
-                    is_ignore_erase_possible = False
-                    break
+            if left_erase_buffer.is_all_overwritten():
+                left_erase_buffer.need_to_ignore = True
 
-            if is_ignore_erase_possible:
-                delete_index_set.add(i)
+        result_buffers = [ x for x in buffers if x.need_to_ignore == False]
+        return self.bufferables_to_list(result_buffers)
 
-        result_buffers = []
-        for index, buffer in enumerate(buffers):
-            if index in delete_index_set:
-                continue
-            result_buffers.append(buffer)
-        return result_buffers
-
-    def divide_erase_range_by_ten(self, erase_buffers):
+    def divide_erase_range_by_max_size(self, erase_buffers: List[EraseBuffer]) -> List[EraseBuffer]:
         result = []
-        for erase_command in erase_buffers:
-            start_address = self.get_lba_from_command(erase_command)
-            erase_size = self.get_count_or_data(erase_command)
+        for buffer in erase_buffers:
+            start_address = buffer.start_addr
+            erase_size = buffer.size
             while erase_size > ERASE_MAX_SIZE:
-                result.append(('E', start_address, ERASE_MAX_SIZE))
+
+                result.append(EraseBuffer('E', start_address, ERASE_MAX_SIZE))
                 erase_size -= ERASE_MAX_SIZE
                 start_address += ERASE_MAX_SIZE
-            result.append(('E', start_address, erase_size))
+            result.append(EraseBuffer('E', start_address, erase_size))
         return result
 
     def merge_erase(self, buffers):
-        size = len(buffers)
-        is_erase_or_write = [None] * (self._device.lba_length + 1)
-        write_data = [None] * (self._device.lba_length + 1)
-        for i in range(0, size):
-            command = buffers[i]
-            op = self.get_op_from_command(command)
-            lba = self.get_lba_from_command(command)
-            count_or_data = self.get_count_or_data(command)
-            if self.is_command_write(command):
-                is_erase_or_write[lba] = op
-                write_data[lba] = count_or_data
-            else:
-                for k in range(0, count_or_data):
-                    is_erase_or_write[lba + k] = op
+        buffer_table = self.create_buffer_table(buffers)
 
         write_buffers = []
         erase_buffers = []
-        index = 0
-        while index < self._device.lba_length:
-            if is_erase_or_write[index] == None:
-                index += 1
-                continue
-            if is_erase_or_write[index] == 'W':
-                write_buffers.append(('W', index, write_data[index]))
-                index += 1
-                continue
-            continuous_end_index = index
-            continuous_erase_end_index = index
-            while is_erase_or_write[continuous_end_index] != None:
-                if is_erase_or_write[continuous_end_index] == 'E':
-                    continuous_erase_end_index = continuous_end_index
-                else:
-                    write_buffers.append(('W', continuous_end_index, write_data[continuous_end_index]))
-                continuous_end_index += 1
-            erase_buffers.append(('E', index, continuous_erase_end_index - index + 1))
-            index = continuous_end_index
-            index += 1
 
-        divided_erase_buffers = self.divide_erase_range_by_ten(erase_buffers)
+        is_erase_section = False
+        erase_start = None
+        erase_end = None
+        for addr, buffer in enumerate(buffer_table):
+            if buffer is None:
+                if is_erase_section:
+                    is_erase_section = False
+                    erase_buffers.append( EraseBuffer('E', erase_start, erase_end-erase_start + 1 ))
+            elif buffer.is_write():
+                write_buffers.append(buffer)
+            elif buffer.is_erase():
+                if not is_erase_section:
+                    is_erase_section = True
+                    erase_start = addr
+                erase_end = addr
+
+        divided_erase_buffers = self.divide_erase_range_by_max_size(erase_buffers)
         result_buffers = divided_erase_buffers + write_buffers
-        return result_buffers
+        return self.bufferables_to_list(result_buffers)
+
+    def create_buffer_table(self, buffers) -> List[Union[WriteBuffer, EraseBuffer, None]]:
+        buffers = self.list_to_bufferables(buffers)
+        buffer_table = [None] * (self._device.lba_length + 1)
+        for buffer in buffers:
+            if buffer.is_write():
+                buffer_table[buffer.addr] = buffer
+            elif buffer.is_erase():
+                for erase_addr in range(buffer.start_addr, buffer.end_addr + 1):
+                    buffer_table[erase_addr] = buffer
+        return buffer_table
 
     def optimize(self):
-        buffers = copy.deepcopy(self._buffers)
+        buffers = copy.deepcopy(self.buffers)
         ignore_write_buffers = self.ignore_write(buffers)
         ignore_erase_buffers = self.ignore_erase(ignore_write_buffers)
         merge_erase_buffers = self.merge_erase(ignore_erase_buffers)
         if len(buffers) > len(merge_erase_buffers):
-            self._buffers = merge_erase_buffers
+            self.buffers = merge_erase_buffers
 
     def read(self, address) -> str:
-        if self.is_exist_in_buffer(address):
-            return self.get_data_in_buffer(address)
-
+        if data := self.get_data_in_buffer(address):
+            return data
         return self._device.read(address)
 
     def write(self, address, wdata):
-        if wdata == DEFAULT_VALUE:
+        if wdata == DEFAULT_DATA:
             self.erase(address, 1)
             return
-        if self.is_buffers_full():
-            self.flush()
-        self._buffers.append(('W', address, wdata))
-        self.optimize()
-        self._driver.make_buffer_files_from_list(self._buffers)
+        self.add_buffer( WriteBuffer('W', address, wdata))
 
     def erase(self, start_address, size):
         if size <= 0:
             return
-        if self.is_buffers_full():
-            self.flush()
-        self._buffers.append(('E', start_address, size))
-        self.optimize()
-        self._driver.make_buffer_files_from_list(self._buffers)
+        self.add_buffer( EraseBuffer('E', start_address, size))
 
     def flush(self):
-        for command in self._buffers:
-            if self.is_command_write(command):
-                self._device.write(command[1], command[2])
-            elif self.is_command_erase(command):
-                self._device.erase(command[1], command[2])
+        for buffer in self._buffers:
+            buffer.execute()
+        self._buffers = []
 
         self._driver.delete_buffer_files()
         self._driver.create_empty_files()
-        self._buffers = []
