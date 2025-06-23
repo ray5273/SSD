789e3342b35f96cb8b7203d2a6a1942558a33c0e
yeonho.jeong <yeonho0701@naver.com>
2025-06-23 15:16:10 +0900

    [feature] command buffer test: ignore command, merge erase, ignore write, erase backward

--- command_buffer_tests/test_command_buffer.py
+++ command_buffer_tests/test_command_buffer.py
@@ -1,5 +1,4 @@
 import pytest
-from pygments.lexers.comal import Comal80Lexer
 
 from buffer_driver import BufferDriver
 from command_buffer import CommandBuffer
@@ -10,7 +9,7 @@ LBA_LENGTH = 100
 DEFAULT_VALUE = "0x00000000"
 FIRST_ADDRESS = 0
 LAST_ADDRESS = LBA_LENGTH - 1
-
+WDATA = "0x1234AAAA"
 
 @pytest.fixture
 def cb_with_fake(mocker):
@@ -36,12 +35,11 @@ def cb_with_fake(mocker):
     driver.create_empty_files = mocker.Mock(side_effect=fake_create)
     driver.get_list_from_buffer_files = mocker.Mock(side_effect=get_buffers)
     driver.make_buffer_files_from_list = mocker.Mock(side_effect=fake_make)
-    # device = Nand(LBA_LENGTH, FILE_PATH)
     device = mocker.Mock(spec=Nand)
     device.lba_length = LBA_LENGTH
     cb = CommandBuffer(device)
     cb._driver = driver
-
+    cb.flush()
     return cb, driver, device
 
 @pytest.fixture
@@ -65,7 +63,6 @@ class TestCommandBufferWithMock:
 
     def test_flush(self, cb_with_fake):
         cb, driver, device = cb_with_fake
-        cb.flush()
         assert len(cb.buffers) == 0
         addr = 5
         wdata = "0xAAAAAAAA"
@@ -75,7 +72,6 @@ class TestCommandBufferWithMock:
 
     def test_write_buffer_length(self, cb_with_fake):
         cb, driver, device = cb_with_fake
-        cb.flush()
         wdata = "0x12345678"
         expected = 1
         for i in range(1,10):
@@ -85,6 +81,92 @@ class TestCommandBufferWithMock:
             if expected > 5:
                 expected  -= 5
 
+    def test_ignore_command(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        wdata = "0xBBBBBBBB"
+        cb.erase(18, 3)
+        cb.write(21, wdata)
+        cb.erase(18, 5)
+        assert len(cb.buffers) == 1
 
+    def test_merge_erase(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        cb.write(20, WDATA)
+        cb.erase(10, 4)
+        cb.erase(12, 3)
+        assert len(cb.buffers) == 2
+
+    def test_merge_big_erase(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        cb.write(20, WDATA)
+        cb.erase(5, 5)
+        cb.erase(15, 5)
+        cb.erase(10, 5)
+        assert len(cb.buffers) == 3
+
+    def test_merge_small_erase(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        cb.erase(5, 3)
+        cb.erase(8, 3)
+        cb.erase(11, 3)
+        assert len(cb.buffers) == 1
+        assert cb.buffers == [('E', 5, 9)]
+        cb.erase(14, 2)
+        assert len(cb.buffers) == 2
+        cb.write(10, WDATA)
+        assert len(cb.buffers) == 3
+
+    def test_write_erase_length(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        cb.erase(5, 5)
+        cb.write(10, WDATA)
+        cb.erase(15, 5)
+        cb.write(20, WDATA)
+        cb.erase(25, 5)
+        assert len(cb.buffers) == 5
+        cb.write(30, WDATA)
+        assert len(cb.buffers) == 1
+
+    def test_read_erased_data(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        cb.write(5, WDATA)
+        cb.erase(5, 1)
+        assert cb.read(5) == DEFAULT_VALUE
+
+
+    def test_ignore_write(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        new_wdata = "0xDDDDDDDD"
+        cb.write(5, WDATA)
+        cb.write(10, WDATA)
+        cb.write(5, new_wdata)
+        assert cb.read(5) == new_wdata
+        assert len(cb.buffers) == 2
+
+    def test_erase_write_data(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        cb.erase(5, 5)
+        cb.write(8, WDATA)
+        cb.erase(5, 5)
+        assert len(cb.buffers) == 1
+
+    def test_erase_backward(self, cb_with_fake):
+        cb, driver, device = cb_with_fake
+        for idx in range(95, 100):
+            cb.write(idx, WDATA)
+        assert cb.read(99) == WDATA
+        cb.erase(99, 1)
+        cb.erase(98, 1)
+        cb.erase(97, 1)
+        cb.erase(96, 1)
+        cb.erase(95, 1)
+        assert cb.read(99) == DEFAULT_VALUE
+        assert cb.buffers == [('E', 95, 5)]
+        cb.erase(95, 5)
+        cb.erase(90, 5)
+        cb.erase(85, 5)
+        cb.erase(80, 5)
+        cb.erase(75, 5)
+        assert len(cb.buffers) == 3
 
 
