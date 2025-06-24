3e947937cf2154145f581f75d8f6a4e9fe001c03
yeonho.jeong <yeonho0701@naver.com>
2025-06-24 09:38:42 +0900

    [refactor] ignore_erase,merge_erase가 argument 없이 class 변수 access 하도록 수정

--- test_optimize_algorithm.py
+++ test_optimize_algorithm.py
@@ -32,33 +32,35 @@ def test_ignore_write2(command_buffer):
 
 def test_ignore_erase(command_buffer):
     buffers = [('E', 0, 2), ('W', 0, '0x12345678'), ('W', 1, '0x11111111'), ('W', 2, '0x11111111')]
-    new_buffers = command_buffer.ignore_erase(buffers)
-    assert new_buffers == [('W', 0, '0x12345678'), ('W', 1, '0x11111111'), ('W', 2, '0x11111111')]
+    command_buffer.buffers = buffers
+    command_buffer.ignore_erase()
+    assert command_buffer.buffers == [('W', 0, '0x12345678'), ('W', 1, '0x11111111'), ('W', 2, '0x11111111')]
 
 
 def test_ignore_erase2(command_buffer):
     buffers = [('E', 0, 2), ('W', 0, '0x12345678'), ('W', 1, '0x11111111'), ('E', 2, 2), ('W', 2, '0x11111111'),
                ('W', 3, '0x11111111')]
-    new_buffers = command_buffer.ignore_erase(buffers)
-    assert new_buffers == [('W', 0, '0x12345678'), ('W', 1, '0x11111111'), ('W', 2, '0x11111111'),
+    command_buffer.buffers = buffers
+    command_buffer.ignore_erase()
+    assert command_buffer.buffers == [('W', 0, '0x12345678'), ('W', 1, '0x11111111'), ('W', 2, '0x11111111'),
                            ('W', 3, '0x11111111')]
 
 
 def test_merge_erase(command_buffer):
-    buffers = [('E', 0, 2), ('E', 2, 2), ('W', 3, '0x11111111')]
-    new_buffers = command_buffer.merge_erase(buffers)
-    assert new_buffers == [('E', 0, 3), ('W', 3, '0x11111111')]
+    command_buffer.buffers = [('E', 0, 2), ('E', 2, 2), ('W', 3, '0x11111111')]
+    command_buffer.merge_erase()
+    assert command_buffer.buffers == [('E', 0, 3), ('W', 3, '0x11111111')]
 
 
 def test_merge_erase2(command_buffer):
-    buffers = [('E', 0, 2), ('W', 2, '0x11111111'), ('W', 3, '0x11111111'), ('E', 4, 3), ('W', 7, '0x11111111')]
-    new_buffers = command_buffer.merge_erase(buffers)
-    assert new_buffers == [('E', 0, 7), ('W', 2, '0x11111111'), ('W', 3, '0x11111111'), ('W', 7, '0x11111111')]
+    command_buffer.buffers = [('E', 0, 2), ('W', 2, '0x11111111'), ('W', 3, '0x11111111'), ('E', 4, 3), ('W', 7, '0x11111111')]
+    command_buffer.merge_erase()
+    assert command_buffer.buffers == [('E', 0, 7), ('W', 2, '0x11111111'), ('W', 3, '0x11111111'), ('W', 7, '0x11111111')]
 
 def test_merge_erase3(command_buffer):
-    buffers = [ ('E', 0, 2), ('E', 5, 3), ('E', 3, 3)]
-    new_buffers = command_buffer.merge_erase(buffers)
-    assert new_buffers == [('E', 0, 2), ('E', 3, 5)]
+    command_buffer.buffers = [ ('E', 0, 2), ('E', 5, 3), ('E', 3, 3)]
+    command_buffer.merge_erase()
+    assert command_buffer.buffers == [('E', 0, 2), ('E', 3, 5)]
 
 def test_total_optimize(command_buffer):
     command_buffer.buffers = [('W', 0, '0x11111111'), ('E', 1, 3), ('W', 4, '0x11111111'), ('E', 4, 7), ('W', 3, '0x11111111')]
@@ -98,7 +100,7 @@ def test_buffer_length(command_buffer):
 def test_divide_by_ten(command_buffer):
     command_buffer.buffers = [('E', 10, 6), ('E', 16, 5)]
     command_buffer.optimize()
-    assert command_buffer.buffers == [('E', 10, 6), ('E', 16, 5)]
+    assert command_buffer.buffers == [('E', 10, 10), ('E', 20, 1)]
 
 def test_divide_by_ten_2(command_buffer):
     command_buffer.buffers = [('E', 10, 8), ('E', 18, 8),('E',26,3),('W',26,'0x11111111'),('E',29,1)]
